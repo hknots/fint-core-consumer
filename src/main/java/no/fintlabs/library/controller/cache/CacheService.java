fb1e710793666bc705d32a2a56bbc166055900ae
Hknots <Hknots99@gmail.com>
2023-10-26 09:47:59 +0200

    have cachemanager create cache

--- src/main/java/no/fintlabs/library/controller/cache/CacheService.java
+++ src/main/java/no/fintlabs/library/controller/cache/CacheService.java
@@ -1,33 +1,43 @@
 package no.fintlabs.library.controller.cache;
 
 import no.fintlabs.cache.Cache;
-import no.fintlabs.cache.CacheObjectFactory;
-import no.fintlabs.cache.FintCache;
+import no.fintlabs.cache.CacheManager;
 import no.fintlabs.cache.packing.PackingTypes;
 import no.fintlabs.library.ReflectionService;
 import no.fintlabs.library.config.ConsumerConfig;
 import no.fintlabs.model.FintMainObject;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class CacheService {
 
+    private final CacheManager cacheManager;
     private final ConsumerConfig consumerConfig;
     private final Map<String, Cache<? extends FintMainObject>> caches;
 
-    public CacheService(ConsumerConfig consumerConfig, ReflectionService reflectionService) {
+    public CacheService(CacheManager cacheManager, ConsumerConfig consumerConfig, ReflectionService reflectionService) {
+        this.cacheManager = cacheManager;
         this.consumerConfig = consumerConfig;
-        caches = initializeCaches(reflectionService.getClassMap());
+        this.caches = initializeCaches(reflectionService.getClassMap());
+    }
+
+    public Cache<? extends FintMainObject> getCache(String resourceName) {
+        return caches.get(resourceName);
     }
 
     private Map<String, Cache<? extends FintMainObject>> initializeCaches(Map<String, Class<? extends FintMainObject>> classMap) {
-        return null;
+        Map<String, Cache<? extends FintMainObject>> tempCaches = new HashMap<>();
+
+        classMap.forEach((clazzName, clazz) -> tempCaches.put(clazzName, createCache(clazzName)));
+
+        return tempCaches;
     }
 
-    private <T extends FintMainObject> FintCache<T> getCache() {
-        new FintCache<>(new CacheObjectFactory<>(PackingTypes.POJO), "urn?");
+    private <T extends FintMainObject> Cache<T> createCache(String resourceName) {
+        return cacheManager.create(PackingTypes.POJO, consumerConfig.getOrgId(), resourceName);
     }
 
 }
