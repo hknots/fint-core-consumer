2fe3bb19f39720061eeffed2b104436f6f3a5b3a
Hknots <hknots99@gmail.com>
2024-02-15 17:25:52 +0100

    add more endpoints

--- src/main/java/no/fintlabs/config/EndPoints.java
+++ src/main/java/no/fintlabs/config/EndPoints.java
@@ -2,6 +2,11 @@ package no.fintlabs.config;
 
 public class EndPoints {
 
-    public static final String COMPONENT_ENDPOINT = "${fint.consumer.domain}/${fint.consumer.package}";
+    public static final String COMPONENT = "${fint.consumer.domain}/${fint.consumer.package}";
+    public static final String RESOURCE = "/{resource}";
+    public static final String RESOURCE_LAST_UPDATED = "/{resource}/last-updated";
+    public static final String RESOURCE_CACHE_SIZE = "/{resource}/cache/size";
+    public static final String RESOURCE_STATUS_ID = "/{resource}/status/{idValue}";
+    public static final String RESOURCE_ID = "/{resource}/{idField}/{idValue}";
 
 }
