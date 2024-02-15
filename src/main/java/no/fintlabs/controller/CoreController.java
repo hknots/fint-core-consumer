19fe85b38bd5355af4707e6c9dab87ed4f966225
Hknots <hknots99@gmail.com>
2024-02-15 16:23:46 +0100

    add base controller

---
+++ src/main/java/no/fintlabs/controller/CoreController.java
@@ -0,0 +1,12 @@
+package no.fintlabs.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import static no.fintlabs.config.EndPoints.COMPONENT_ENDPOINT;
+
+@RestController(COMPONENT_ENDPOINT)
+public class CoreController {
+
+
+
+}
