7e5a1dfcfec68b6d9bbb0611da66f863c9d5a563
Hknots <hknots99@gmail.com>
2024-02-15 15:17:36 +0100

    initial commit

---
+++ src/main/java/no/fintlabs/FintCoreConsumerApplication.java
@@ -0,0 +1,13 @@
+package no.fintlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintCoreConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintCoreConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/fintlabs/FintCoreConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package no.fintlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintCoreConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
