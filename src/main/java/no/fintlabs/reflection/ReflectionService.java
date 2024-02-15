4c90e9709293e78ecbee9c8fe1be480d21234b06
Hknots <hknots99@gmail.com>
2024-02-15 16:24:31 +0100

    add bean for resources

--- src/main/java/no/fintlabs/reflection/ReflectionService.java
+++ src/main/java/no/fintlabs/reflection/ReflectionService.java
@@ -1,22 +1,28 @@
 package no.fintlabs.reflection;
 
+import lombok.RequiredArgsConstructor;
+import no.fint.model.FintMainObject;
 import no.fintlabs.config.ConsumerConfig;
 import org.reflections.Reflections;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 
 @Service
+@RequiredArgsConstructor
 public class ReflectionService {
 
     private final ConsumerConfig props;
 
-    public ReflectionService(ConsumerConfig props) {
-        this.props = props;
-        Reflections reflection = new Reflections(getFullPackageName());
-    }
-
     private String getFullPackageName() {
         return String.format("no.fint.model.%s.%s", props.getDomainName(), props.getPackageName());
+    }
+
+    @Bean
+    public Set<Class<? extends FintMainObject>> resources() {
+        return new Reflections(getFullPackageName()).getSubTypesOf(FintMainObject.class);
     }
 
 }
