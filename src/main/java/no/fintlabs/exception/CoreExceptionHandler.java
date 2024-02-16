06c886c4b3c594b32f30ea42548037c75870affc
Hknots <hknots99@gmail.com>
2024-02-16 18:43:24 +0100

    add IdentificatorFieldNotFound to ExceptionHandler

--- src/main/java/no/fintlabs/exception/CoreExceptionHandler.java
+++ src/main/java/no/fintlabs/exception/CoreExceptionHandler.java
@@ -1,5 +1,6 @@
 package no.fintlabs.exception;
 
+import no.fintlabs.exception.exceptions.IdentificatorFieldNotFound;
 import no.fintlabs.exception.exceptions.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class CoreExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
+    @ExceptionHandler({ResourceNotFoundException.class, IdentificatorFieldNotFound.class})
     public ResponseEntity<Void> handleResourceNotFoundException() {
         return ResponseEntity.notFound().build();
     }
